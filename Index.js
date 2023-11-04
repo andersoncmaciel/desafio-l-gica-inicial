4aa75a65052de869a80d9049d97b557e8879e211
ANDERSON CARLOS MACIEL <andersoncmaciel@gmail.com>
2023-11-04 12:48:18 -0300

    Update Index.js
    Errei o primeiro envio. Então esta é uma correção na apresentação do código final.

--- Index.js
+++ Index.js
@@ -1,5 +1,29 @@
-let x = 10;
+let nome = "Anderson";
+let nivel = "9500";
 
-if (x > 5) {
-    console.log("Olá mundo!")
+switch (true) {
+  case nivel < 1000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Ferro"}!`); 
+    break; 
+  case nivel >= 1001 && nivel <= 2000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Bronze"}!`);  
+    break;
+  case nivel >= 2001 && nivel <= 5000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Prata"}!`);  
+    break;
+  case nivel >= 5001 && nivel <=6000:
+    console.log(`O herói de nome ${nome} está no nível de  ${"Ouro"}!`);  
+    break;
+  case nivel >= 6001 && nivel <= 7000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Platina"}!`);  
+    break;
+  case nivel >= 7001 && nivel <= 8000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Ascendente"}!`);  
+    break;
+  case nivel >= 8001 && nivel <= 9000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Imortal"}!`);  
+    break;
+  case nivel >= 9001 && nivel <= 10000:
+    console.log(`O herói de nome ${nome} está no nível de ${"Radiante"}!`);  
+    break;
 }
